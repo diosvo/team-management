3d714e17dbaa965701a758788abe7503032ed1ba
Dios Vo <vtmn1212@gmail.com>
2025-03-09 17:49:42 +0700

    Set loading component and remove responsive provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,9 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { Provider as UiProvider } from '@/components/ui/provider';
 import { Toaster } from '@/components/ui/toaster';
-import { ResponsiveProvider } from '@/contexts/responsive-provider';
 
 import './globals.css';
+import Loading from './loading';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -35,9 +35,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <UiProvider>
           <Toaster />
-          <ResponsiveProvider>
-            <Suspense fallback={<>Loading...</>}>{children}</Suspense>
-          </ResponsiveProvider>
+          <Suspense fallback={<Loading />}>{children}</Suspense>
         </UiProvider>
         {/* Vercel plugins */}
         {process.env.NODE_ENV === 'production' && (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,14 +15,12 @@ import {
 import TextEditor from '@/components/text-editor';
 import { ColorModeButton } from '@/components/ui/color-mode';
 import { toaster } from '@/components/ui/toaster';
-import { useResponsive } from '@/contexts/responsive-provider';
 
 import { executeRule, getRule } from '@/features/rule/actions/rule';
 import { RuleValues } from '@/features/rule/schemas/rule';
 import Link from 'next/link';
 
 export default function MainPage() {
-  const { isMobile, isTablet, isDesktop } = useResponsive();
   const [content, setContent] = useState('');
   const [userRole, setUserRole] = useState('read'); // For demo
   const [isEditing, setIsEditing] = useState(false);
@@ -47,12 +45,6 @@ export default function MainPage() {
           Login
         </Link>
       </Button>
-
-      <div>
-        {isMobile && 'mobile'}
-        {isTablet && 'tablet'}
-        {isDesktop && 'desktop'}
-      </div>
 
       <Text fontSize={['sm', 'md', 'lg', 'xl']}>
         This text adapts across breakpoints
