604d64fda2a4db8aed40103567de61d602290b0a
Dios Vo <vtmn1212@gmail.com>
2025-04-20 10:36:10 +0700

    Optimize with snippets

--- src/app/(protected)/_components/team-rule.tsx
+++ src/app/(protected)/_components/team-rule.tsx
@@ -2,16 +2,27 @@
 
 import { useState, useTransition } from 'react';
 
-import { Button, Dialog, Icon, Portal, Text } from '@chakra-ui/react';
+import { Button, Icon, Text } from '@chakra-ui/react';
 import { Crown, Eye, Pencil } from 'lucide-react';
 
 import TextEditor from '@/components/text-editor';
-import { CloseButton } from '@/components/ui/close-button';
+import {
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import { toaster } from '@/components/ui/toaster';
 import { Tooltip } from '@/components/ui/tooltip';
 import Visibility from '@/components/visibility';
 
 import { Rule } from '@/drizzle/schema';
+import { LOCALE } from '@/utils/constant';
+
 import { executeRule } from '@/features/rule/actions/rule';
 import { RuleValues } from '@/features/rule/schemas/rule';
 
@@ -44,60 +55,51 @@ export default function TeamRule({ editable, team_id, rule }: TeamRuleProps) {
   }
 
   return (
-    <Dialog.Root size="lg">
-      <Dialog.Trigger asChild>
+    <DialogRoot size="lg">
+      <DialogTrigger asChild>
         <Button size="sm" variant="ghost" justifyContent="flex-start">
           <Icon as={Crown} color="orange.focusRing" />
           Team Rule
         </Button>
-      </Dialog.Trigger>
-      <Portal>
-        <Dialog.Backdrop />
-        <Dialog.Positioner>
-          <Dialog.Content>
-            <Dialog.Header>
-              <Dialog.Title display="flex" alignItems="baseline" gap={1}>
-                <Text>Regulation</Text>
-                <Visibility isVisible={editable}>
-                  <Tooltip
-                    content={`${isEditing ? 'Preview' : 'Enable'} editing`}
-                  >
-                    <Button
-                      size="2xs"
-                      variant="ghost"
-                      onClick={() => setIsEditing(!isEditing)}
-                    >
-                      {isEditing ? <Eye /> : <Pencil />}
-                    </Button>
-                  </Tooltip>
-                </Visibility>
-              </Dialog.Title>
-            </Dialog.Header>
-            <Dialog.Body>
-              <TextEditor
-                editable={editable && isEditing && !isPending}
-                loading={isPending}
-                content={rule.content as string}
-                onSave={async (content: string) =>
-                  await onSubmit({
-                    content,
-                    team_id,
-                  })
-                }
-              />
-            </Dialog.Body>
-            <Dialog.Footer justifyContent="flex-start">
-              <Text fontSize="xs" color="gray.500">
-                {rule.updated_at &&
-                  `Last updated on ${rule.updated_at.toLocaleString('vi-VN')}`}
-              </Text>
-            </Dialog.Footer>
-            <Dialog.CloseTrigger asChild>
-              <CloseButton size="sm" />
-            </Dialog.CloseTrigger>
-          </Dialog.Content>
-        </Dialog.Positioner>
-      </Portal>
-    </Dialog.Root>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle display="flex" alignItems="baseline" gap={1}>
+            <Text>Regulation</Text>
+            <Visibility isVisible={editable}>
+              <Tooltip content={`${isEditing ? 'Preview' : 'Enable'} editing`}>
+                <Button
+                  size="2xs"
+                  variant="ghost"
+                  onClick={() => setIsEditing(!isEditing)}
+                >
+                  {isEditing ? <Eye /> : <Pencil />}
+                </Button>
+              </Tooltip>
+            </Visibility>
+          </DialogTitle>
+        </DialogHeader>
+        <DialogBody>
+          <TextEditor
+            editable={editable && isEditing && !isPending}
+            loading={isPending}
+            content={rule.content as string}
+            onSave={async (content: string) =>
+              await onSubmit({
+                content,
+                team_id,
+              })
+            }
+          />
+        </DialogBody>
+        <DialogFooter justifyContent="flex-start">
+          <Text fontSize="xs" color="gray.500">
+            {rule.updated_at &&
+              `Last updated on ${rule.updated_at.toLocaleString(LOCALE)}`}
+          </Text>
+        </DialogFooter>
+        <DialogCloseTrigger />
+      </DialogContent>
+    </DialogRoot>
   );
 }
