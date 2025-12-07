b617cb951a7e56b03b10a371752df51e8006f819
Dios Vo <vtmn1212@gmail.com>
2025-12-07 14:05:06 +0700

    Enhance SearchInput with focusable function.

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,46 +1,41 @@
 'use client';
 
-import { Input, InputGroup } from '@chakra-ui/react';
+import { useRef } from 'react';
+
+import { Input, InputGroup, InputProps } from '@chakra-ui/react';
 import { Search } from 'lucide-react';
 
 import { CloseButton } from '@/components/ui/close-button';
 import { useCommonParams } from '@/utils/filters';
 
-type SearchInputProps = Partial<{
-  disabled: boolean;
-  maxLength: number;
-}>;
-
-export default function SearchInput({
-  disabled = false,
-  maxLength = 64,
-}: SearchInputProps) {
+export default function SearchInput(props: InputProps) {
   const [{ q }, setSearchParams] = useCommonParams();
+  const inputRef = useRef<Nullable<HTMLInputElement>>(null);
+
+  const endElement = q ? (
+    <CloseButton
+      size="2xs"
+      borderRadius="full"
+      onClick={() => {
+        setSearchParams({ q: '' });
+        inputRef.current?.focus();
+      }}
+    />
+  ) : undefined;
 
   return (
-    <InputGroup
-      startElement={<Search size={14} />}
-      endElement={
-        q && (
-          <CloseButton
-            size="2xs"
-            borderRadius="full"
-            onClick={() => setSearchParams({ q: '' })}
-          />
-        )
-      }
-    >
+    <InputGroup startElement={<Search size={14} />} endElement={endElement}>
       <Input
         type="search"
         value={q}
-        disabled={disabled}
-        maxLength={maxLength}
+        ref={inputRef}
         name="search-input"
         placeholder="Search..."
         borderWidth={1}
         size={{ base: 'sm', md: 'md' }}
         css={{ '--focus-color': 'colors.red.300' }}
         onChange={(e) => setSearchParams({ q: e.target.value })}
+        {...props}
       />
     </InputGroup>
   );
